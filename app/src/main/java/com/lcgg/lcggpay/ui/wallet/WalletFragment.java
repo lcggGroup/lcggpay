15ccb529ac3bfa2865fa3df2e885bb4c39bed8a5
lcggGroup <lcgg.group@gmail.com>
2021-02-17 22:32:56 +0800

    Initial Commit - feature branch - added wallet menu

---
+++ app/src/main/java/com/lcgg/lcggpay/ui/wallet/WalletFragment.java
@@ -0,0 +1,36 @@
+package com.lcgg.lcggpay.ui.wallet;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.lcgg.lcggpay.R;
+import com.lcgg.lcggpay.ui.pay.PayViewModel;
+
+public class WalletFragment extends Fragment {
+
+    private PayViewModel payViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        payViewModel =
+                new ViewModelProvider(this).get(PayViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_pay, container, false);
+        final TextView textView = root.findViewById(R.id.text_pay);
+        payViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
