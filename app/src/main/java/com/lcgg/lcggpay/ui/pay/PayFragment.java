f06564f68291409fcdcbf05f67206b07733438c1
lcggGroup <lcgg.group@gmail.com>
2021-02-18 15:10:26 +0800

    Initial Commit - test branch - qr code reader

--- app/src/main/java/com/lcgg/lcggpay/ui/pay/PayFragment.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/pay/PayFragment.java
@@ -1,5 +1,8 @@
 package com.lcgg.lcggpay.ui.pay;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,23 +11,32 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.lcgg.lcggpay.R;
 import com.lcgg.lcggpay.ui.dashboard.DashboardViewModel;
 
 public class PayFragment extends Fragment {
 
     private PayViewModel payViewModel;
+    private TextView textView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this.getActivity());
+        intentIntegrator.initiateScan();
+
         payViewModel =
                 new ViewModelProvider(this).get(PayViewModel.class);
         View root = inflater.inflate(R.layout.fragment_pay, container, false);
-        final TextView textView = root.findViewById(R.id.text_pay);
+        textView = root.findViewById(R.id.text_pay);
         payViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -32,5 +44,22 @@ public class PayFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult intentResult =IntentIntegrator.parseActivityResult(requestCode,resultCode, data);
+
+        if (intentResult != null){
+            if (intentResult.getContents() != null){
+                textView.setText("Cancelled");
+            }
+            else {
+                textView.setText(intentResult.getContents());
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
