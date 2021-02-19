72464f9712baf68424b326d5e84c16a5cb7d9099
lcggGroup <lcgg.group@gmail.com>
2021-02-19 15:01:07 +0800

    Initial Commit - test branch - update bottom navigation

--- app/src/main/java/com/lcgg/lcggpay/MainActivity.java
+++ app/src/main/java/com/lcgg/lcggpay/MainActivity.java
@@ -3,8 +3,12 @@ package com.lcgg.lcggpay;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -18,28 +22,45 @@ import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.navigation_home:
+                        navController.navigate(R.id.navigation_home);
+                    case R.id.navigation_pay:
+                        navController.navigate(R.id.navigation_pay);
+                    case R.id.navigation_store:
+                        navController.navigate(R.id.navigation_store);
+                    case R.id.navigation_wallet:
+                        navController.navigate(R.id.navigation_wallet);
+                }
+                return true;
+            }
+        });
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_pay, R.id.navigation_store, R.id.navigation_wallet)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
         AppCenter.start(getApplication(), "a0dfaf0e-a308-4fde-bb0d-c83219f54eb3",
                 Analytics.class, Crashes.class);
 
-        //login();
-
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
