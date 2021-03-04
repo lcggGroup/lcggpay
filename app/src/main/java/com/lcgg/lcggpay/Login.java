22551c819252e3081ce3928cedf8fd1de35ab3ee
lcggGroup <lcgg.group@gmail.com>
2021-03-04 23:42:45 +0800

    Initial Release Login Activity - added firebase auth

--- app/src/main/java/com/lcgg/lcggpay/Login.java
+++ app/src/main/java/com/lcgg/lcggpay/Login.java
@@ -1,17 +1,32 @@
 package com.lcgg.lcggpay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.lcgg.lcggpay.data.model.PayPal;
 import com.paypal.android.sdk.payments.PayPalService;
 
 public class Login extends AppCompatActivity {
+
+    private static final String TAG = "FragmentActivity";
+    private FirebaseAuth mAuth;
+
+    EditText txt_username;
+    EditText txt_password;
 
     Button btnSubmit;
     Button btnCancel;
@@ -21,14 +36,42 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        txt_username = findViewById(R.id.txt_username);
+        txt_password = findViewById(R.id.txt_password);
+
         btnSubmit = findViewById(R.id.btn_submit);
         btnCancel = findViewById(R.id.btn_cancel);
+
+
+
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this, MainActivity.class);
-                startActivity(intent);
+
+                mAuth.createUserWithEmailAndPassword(txt_username.getText().toString(), txt_password.getText().toString())
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                    Intent intent = new Intent(Login.this, MainActivity.class);
+                                    startActivity(intent);
+                                    //updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(Login.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+                            }
+                        });
             }
         });
 
@@ -38,5 +81,15 @@ public class Login extends AppCompatActivity {
                 Toast.makeText(Login.this, "Cancelled", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //reload();
+        }
     }
 }
