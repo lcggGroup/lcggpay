f87a275bfe8e1b7b85fe70a7d75cbf7540b81d6a
lcggGroup <lcgg.group@gmail.com>
2021-03-04 22:42:55 +0800

    Initial Release Login Activity - added button

--- app/src/main/java/com/lcgg/lcggpay/Login.java
+++ app/src/main/java/com/lcgg/lcggpay/Login.java
@@ -2,13 +2,41 @@ package com.lcgg.lcggpay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.lcgg.lcggpay.data.model.PayPal;
+import com.paypal.android.sdk.payments.PayPalService;
 
 public class Login extends AppCompatActivity {
+
+    Button btnSubmit;
+    Button btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSubmit = findViewById(R.id.btn_submit);
+        btnCancel = findViewById(R.id.btn_cancel);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startService(intent);
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Login.this, "Cancelled", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
