e346bd09a5df57fd4610fd75f93dfcc98f52c3f5
lcggGroup <lcgg.group@gmail.com>
2021-03-04 22:51:55 +0800

    Initial Release Login Activity - update submit button

--- app/src/main/java/com/lcgg/lcggpay/Login.java
+++ app/src/main/java/com/lcgg/lcggpay/Login.java
@@ -28,7 +28,7 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this, MainActivity.class);
-                startService(intent);
+                startActivity(intent);
             }
         });
 
