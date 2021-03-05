72628c0212882e16c14536d18f131afcf398c091
lcggGroup <lcgg.group@gmail.com>
2021-03-05 22:58:23 +0800

    Initial Release Login Activity - Register Activity

--- app/src/main/java/com/lcgg/lcggpay/Register.java
+++ app/src/main/java/com/lcgg/lcggpay/Register.java
@@ -1,14 +1,30 @@
 package com.lcgg.lcggpay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+
+    EditText txt_reg_username;
+    EditText txt_reg_password;
+
+    Button btnSubmit;
+    Button btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +32,65 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance();
+
+        txt_reg_username = findViewById(R.id.txt_reg_username);
+        txt_reg_password = findViewById(R.id.txt_reg_username);
+
+        btnSubmit = findViewById(R.id.btn_reg_submit);
+        btnCancel = findViewById(R.id.btn_reg_cancel);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(txt_reg_username.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(txt_reg_password.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (txt_reg_password.getText().toString().length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(txt_reg_username.getText().toString(), txt_reg_password.getText().toString())
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    btnSubmit.setVisibility(View.GONE);
+                                    btnCancel.setText("Done");
+                                    btnCancel.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            startActivity(new Intent(Register.this, MainActivity.class));
+                                            finish();
+                                        }
+                                    });
+
+                                }
+                            }
+                        });
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
