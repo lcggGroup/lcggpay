17b412030c85ddfd448f51968641e6b153051240
lcggGroup <lcgg.group@gmail.com>
2021-03-08 13:32:40 +0800

    Initial Release - Added Login Activity - Added Registration - Added Forgot Password - Added Pay Activity - Update Manifest (Remove Camera Permissions)

--- app/src/main/java/com/lcgg/lcggpay/ui/pay/PayFragment.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/pay/PayFragment.java
@@ -8,134 +8,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
+
 import com.lcgg.lcggpay.R;
-import com.lcgg.lcggpay.confirmation.PaymentDetailsActivity;
-import com.lcgg.lcggpay.confirmation.PaymentDetailsActivity;
-import com.lcgg.lcggpay.data.model.PayPal;
-import com.paypal.android.sdk.payments.PayPalPayment;
-import com.paypal.android.sdk.payments.PaymentActivity;
-import com.paypal.android.sdk.payments.PayPalService;
-import com.paypal.android.sdk.payments.PayPalConfiguration;
-import com.paypal.android.sdk.payments.PaymentConfirmation;
-
-import org.json.JSONException;
-
-import java.math.BigDecimal;
 
 import static android.app.Activity.RESULT_OK;
 
 public class PayFragment extends Fragment {
-    Button btnConfirm;
-    Button btnCancel;
-
-    TextView txt_title;
-    TextView txt_amount;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
-        IntentIntegrator intentIntegrator = IntentIntegrator.forSupportFragment(PayFragment.this);
-        intentIntegrator.setPrompt("");
-        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-        intentIntegrator.initiateScan();
 
         View root = inflater.inflate(R.layout.fragment_pay, container, false);
-
-        btnConfirm = root.findViewById(R.id.btn_confirm);
-        btnCancel = root.findViewById(R.id.btn_cancel);
-
-        txt_title = root.findViewById(R.id.txt_title);
-        txt_amount = root.findViewById(R.id.txt_amount);
-
-        //start paypal service
-        Intent intent = new Intent(getActivity(),PayPalService.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, PayPal.PAYPAL_CONFIG);
-        this.getActivity().startService(intent);
-
-        btnConfirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                processPayment();
-            }
-        });
-
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "Cancelled", Toast.LENGTH_SHORT).show();
-            }
-        });
 
         return root;
     }
 
-    private void processPayment() {
-        PayPalPayment payPalPayment = new PayPalPayment(new BigDecimal(txt_amount.getText().toString()),"USD",
-                txt_title.getText().toString(), PayPalPayment.PAYMENT_INTENT_SALE);
-        Intent intent = new Intent(getActivity(), PaymentActivity.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, PayPal.PAYPAL_CONFIG);
-        intent.putExtra(com.paypal.android.sdk.payments.PaymentActivity.EXTRA_PAYMENT,payPalPayment);
-        this.getActivity().startActivityForResult(intent, PayPal.PAYPAL_REQUEST_CODE);
-    }
-
-    @Override
-    public void onDestroy() {
-        this.getActivity().stopService(new Intent(this.getActivity(), PayPalService.class));
-        super.onDestroy();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if (result.getContents() == null) {
-                //Cancel Scan
-                Toast.makeText(getContext(), "Cancelled", Toast.LENGTH_SHORT).show();
-            }
-            else {
-                //Successful Scan
-                txt_title.setText("Sample Title");
-                txt_amount.setText("15");
-            }
-        }
-        else {
-            super.onActivityResult(requestCode, resultCode, data);
-
-            if (requestCode == PayPal.PAYPAL_REQUEST_CODE) {
-                if (resultCode == RESULT_OK) {
-                    PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
-                    if (confirmation != null) {
-                        try {
-                            String paymentDetails = confirmation.toJSONObject().toString(4);
-
-                            this.getActivity().startActivity(new Intent(getActivity(), PaymentDetailsActivity.class)
-                                    .putExtra("PaymentDetails", paymentDetails)
-                                    .putExtra("PaymentAmout", txt_amount.getText())
-                            );
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-                else if (resultCode == Activity.RESULT_CANCELED)
-                    Toast.makeText(this.getActivity(), "Cancel", Toast.LENGTH_SHORT).show();
-                else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID)
-                    Toast.makeText(this.getActivity(), "Invalid", Toast.LENGTH_SHORT).show();
-            }
-        }
-
-    }
-}
+  }
