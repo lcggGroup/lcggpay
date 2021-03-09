82cf75d3b96cae3b0b98da129e3d425cb28ffd43
lcggGroup <lcgg.group@gmail.com>
2021-03-09 17:52:04 +0800

    Initial Release - Added Wallet Class - Added Login Activity - Added Registration   >> Added Wallet - Added Forgot Password - Update Manifest   >> Added Camera Permission   >> Added Internet Permission   >> Added Read Phone State Permission

---
+++ app/src/main/java/com/lcgg/lcggpay/Wallet.java
@@ -0,0 +1,16 @@
+package com.lcgg.lcggpay;
+
+public class Wallet {
+    public String description;
+    public String amount;
+
+    public Wallet() {
+        // Default constructor required for calls to DataSnapshot.getValue(Profile.class)
+    }
+
+    public Wallet(String description, String amount) {
+        this.description = description;
+        this.amount = amount;
+    }
+
+}
