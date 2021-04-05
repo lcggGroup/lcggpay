00b0e1f2f83bfc5d26f489eff5d24077011e28c8
lcggGroup <lcgg.group@gmail.com>
2021-04-05 15:56:36 +0800

    Initial Release - Added Login - Added Registration - Added Forgot Password - Added Top Menu - Added Profile Page - Added Payment Page - Added Wallet Page

--- app/src/main/java/com/lcgg/lcggpay/ui/wallet/WalletFragment.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/wallet/WalletFragment.java
@@ -7,21 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.lcgg.lcggpay.R;
 import com.lcgg.lcggpay.Wallet;
 
@@ -31,17 +22,11 @@ public class WalletFragment extends Fragment {
     Button addFunds;
     Button transferFunds;
     Button withdrawFunds;
-    Intent intent;
-
-    private FirebaseAuth mAuth;
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private DatabaseReference myRef = database.getReference(mAuth.getUid());
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_wallet, container, false);
-        mAuth = FirebaseAuth.getInstance();
 
         txtBalance = root.findViewById(R.id.wallet_balance);
 
