852b2b9e1bf84ab1898a50c645f8bf7ca9e71cc0
lcggGroup <lcgg.group@gmail.com>
2021-04-06 17:18:09 +0800

    Bug Fixes - Updated Scan to Pay - Updated Wallet Amount - Updated Fund Transfers - Updated Profile
    Initial Release
    - Added Login
    - Added Registration
    - Added Forgot Password
    - Added Top Menu
    - Added Profile Page
    - Added Payment Page
    - Added Wallet Page

--- app/src/main/java/com/lcgg/lcggpay/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/profile/ProfileFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,10 +14,18 @@ import com.lcgg.lcggpay.R;
 
 public class ProfileFragment extends Fragment {
 
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X"};
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), R.layout.fragment_profile, mobileArray);
+
+        ListView listView = (ListView) root.findViewById(R.id.simpleListView);
+        listView.setAdapter(adapter);
 
         return root;
     }
