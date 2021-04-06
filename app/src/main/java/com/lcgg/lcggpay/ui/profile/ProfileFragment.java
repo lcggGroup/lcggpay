dff3d99583d1fb9c7ed4b9ceb2edaf1f4dbc69b3
lcggGroup <lcgg.group@gmail.com>
2021-04-06 18:21:48 +0800

    Bug Fixes - Updated Scan to Pay - Updated Wallet Amount - Updated Fund Transfers - Updated Profile
    Initial Release
    - Added Login
    - Added Registration
    - Added Forgot Password
    - Added Top Menu
    - Added Profile Page
    - Added Payment Page
    - Added Wallet Page

--- app/src/main/java/com/lcgg/lcggpay/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/profile/ProfileFragment.java
@@ -1,7 +1,10 @@
 package com.lcgg.lcggpay.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -11,9 +14,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.lcgg.lcggpay.Login;
+import com.lcgg.lcggpay.MainActivity;
+import com.lcgg.lcggpay.Profile;
 import com.lcgg.lcggpay.R;
 
 public class ProfileFragment extends AppCompatActivity {
+    Intent intent;
+    private FirebaseAuth mAuth;
 
     String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
             "WebOS","Ubuntu","Windows7","Max OS X"};
@@ -32,5 +41,35 @@ public class ProfileFragment extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.simpleListView);
         listView.setAdapter(adapter);
         //return root;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.navigation_home:
+                //add the function to perform here
+                //getSupportFragmentManager().beginTransaction()
+                //.replace(R.id.nav_host_fragment, new ProfileFragment())
+                //.commit();
+                intent = new Intent(ProfileFragment.this, MainActivity.class);
+                startActivity(intent);
+                return(true);
+
+            case R.id.exit:
+                //add the function to perform here
+                mAuth.getInstance().signOut();
+                intent = new Intent(ProfileFragment.this, Login.class);
+                startActivity(intent);
+                finish();
+                return(true);
+        }
+        return(super.onOptionsItemSelected(item));
     }
 }
