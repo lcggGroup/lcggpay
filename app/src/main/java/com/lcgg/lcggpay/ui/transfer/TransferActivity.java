8d8e8a5e8e0e17eee1bcb3eb7f3b28b148b13222
lcggGroup <lcgg.group@gmail.com>
2021-07-30 23:14:54 +0800

    Bug Fixes - Transfer Page link
    Initial Release
    - Added Login
    - Added Registration
    - Added Forgot Password
    - Added Top Menu
    - Added Profile Page
    - Added Payment Page
    - Added Wallet Page
    - Added Transfer Page

--- app/src/main/java/com/lcgg/lcggpay/ui/transfer/TransferActivity.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/transfer/TransferActivity.java
@@ -1,19 +1,7 @@
 package com.lcgg.lcggpay.ui.transfer;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.lcgg.lcggpay.Login;
 import com.lcgg.lcggpay.R;
 
 public class TransferActivity extends AppCompatActivity {
