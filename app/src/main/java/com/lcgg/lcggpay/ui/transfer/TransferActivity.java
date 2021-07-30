ca2dedc0d42bcfa06645f55472c029e5a0c7ea14
lcggGroup <lcgg.group@gmail.com>
2021-07-31 00:09:45 +0800

    Bug Fixes - Transfer Page
    Initial Release
    - Added Login
    - Added Registration
    - Added Forgot Password
    - Added Top Menu
    - Added Profile Page
    - Added Payment Page
    - Added Wallet Page
    - Added Transfer Page

--- app/src/main/java/com/lcgg/lcggpay/ui/transfer/TransferActivity.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/transfer/TransferActivity.java
@@ -1,14 +1,43 @@
 package com.lcgg.lcggpay.ui.transfer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.lcgg.lcggpay.MainActivity;
 import com.lcgg.lcggpay.R;
 
 public class TransferActivity extends AppCompatActivity {
 
+    Intent intent;
+
+    Button cancel;
+    Button submit;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer);
+
+        cancel = findViewById(R.id.transferBtn_cancel);
+        submit = findViewById(R.id.transferBtn_submit);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(TransferActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
