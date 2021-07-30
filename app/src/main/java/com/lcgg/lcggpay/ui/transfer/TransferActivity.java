a76a1c98b482001e37b0f0d15b03651c99d898bf
lcggGroup <lcgg.group@gmail.com>
2021-07-30 22:43:35 +0800

    Bug Fixes - Transfer Page link
    Initial Release
    - Added Login
    - Added Registration
    - Added Forgot Password
    - Added Top Menu
    - Added Profile Page
    - Added Payment Page
    - Added Wallet Page
    - Added Transfer Page

--- app/src/main/java/com/lcgg/lcggpay/ui/transfer/TransferActivity.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/transfer/TransferActivity.java
@@ -11,25 +11,63 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.lcgg.lcggpay.Login;
 import com.lcgg.lcggpay.R;
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
 
 public class TransferActivity extends AppCompatActivity {
+    Intent intent;
+    String[] mobileArray = { "Username and Password","Contact Information" };
+    private FirebaseAuth mAuth;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transfer);
+        setContentView(R.layout.fragment_profile);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(TransferActivity.this, R.layout.list_view, mobileArray);
+        ListView listView = (ListView) findViewById(R.id.simpleListView);
+        listView.setAdapter(adapter);
+
+        //return root;
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0) {
+                    //code specific to first list item
+                    Toast.makeText(TransferActivity.this, "1" , Toast.LENGTH_SHORT).show();
+                    //create intent - UserName and Password
+                }
+                else if(position == 1) {
+                    //code specific to first list item
+                    Toast.makeText(TransferActivity.this, "2" , Toast.LENGTH_SHORT).show();
+                    //create intent - Contact Information
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_nav_menu_profile, menu);
+        //return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.exit:
+                //add the function to perform here
+                mAuth.getInstance().signOut();
+                intent = new Intent(TransferActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+                return(true);
+        }
+        return(super.onOptionsItemSelected(item));
     }
 }
