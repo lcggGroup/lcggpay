62ab358a9ef170aa3db47c97bd011998e2e23387
lcggGroup <lcgg.group@gmail.com>
2021-07-30 21:11:39 +0800

    Bug Fixes - Transfer Page link
    Initial Release
    - Added Login
    - Added Registration
    - Added Forgot Password
    - Added Top Menu
    - Added Profile Page
    - Added Payment Page
    - Added Wallet Page
    - Added Transfer Page

--- app/src/main/java/com/lcgg/lcggpay/ui/transfer/TransferActivity.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/transfer/TransferActivity.java
@@ -17,31 +17,10 @@ import com.lcgg.lcggpay.Login;
 import com.lcgg.lcggpay.R;
 
 public class TransferActivity extends AppCompatActivity {
-    Intent intent;
-    private FirebaseAuth mAuth;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_transfer);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.top_nav_menu_profile, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.exit:
-                //add the function to perform here
-                mAuth.getInstance().signOut();
-                intent = new Intent(TransferActivity.this, Login.class);
-                startActivity(intent);
-                finish();
-                return(true);
-        }
-        return(super.onOptionsItemSelected(item));
-    }
 }
