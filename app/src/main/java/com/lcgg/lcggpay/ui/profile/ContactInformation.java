ca9bec5bd5bc7dfd28dba09e8924ea693b2a25a7
lcggGroup <lcgg.group@gmail.com>
2021-08-04 01:36:16 +0800

    Bug Fixes - Profile Page
    Initial Release
    - Added Login
    - Added Registration
    - Added Forgot Password
    - Added Top Menu
    - Added Profile Page
    - Added Payment Page
    - Added Wallet Page
    - Added Transfer Page

---
+++ app/src/main/java/com/lcgg/lcggpay/ui/profile/ContactInformation.java
@@ -0,0 +1,24 @@
+package com.lcgg.lcggpay.ui.profile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.lcgg.lcggpay.Login;
+import com.lcgg.lcggpay.R;
+
+public class ContactInformation extends AppCompatActivity {
+    Intent intent;
+    private FirebaseAuth mAuth;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_info);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
