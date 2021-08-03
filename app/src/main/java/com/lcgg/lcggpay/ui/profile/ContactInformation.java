438557beb1e16c550aec5ee032d883a58c4e4f6a
lcggGroup <lcgg.group@gmail.com>
2021-08-04 05:54:58 +0800

    Bug Fixes - Profile Page
    Initial Release
    - Added Login
    - Added Registration
    - Added Forgot Password
    - Added Top Menu
    - Added Profile Page
    - Added Payment Page
    - Added Wallet Page
    - Added Transfer Page

--- app/src/main/java/com/lcgg/lcggpay/ui/profile/ContactInformation.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/profile/ContactInformation.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,14 +16,63 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.lcgg.lcggpay.Login;
 import com.lcgg.lcggpay.R;
 
+import org.w3c.dom.Text;
+
 public class ContactInformation extends AppCompatActivity {
-    Intent intent;
-    private FirebaseAuth mAuth;
+    Button edit;
+
+    TextView fName;
+    TextView mName;
+    TextView lName;
+
+    EditText fNameEdit;
+    EditText mNameEdit;
+    EditText lNameEdit;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_info);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        edit = findViewById(R.id.contactBtn_edit);
+
+        fName = findViewById(R.id.db_contact_fn);
+        mName = findViewById(R.id.db_contact_mn);
+        lName = findViewById(R.id.db_contact_ln);
+
+        fNameEdit = findViewById(R.id.txt_contact_ln);
+        mNameEdit = findViewById(R.id.txt_contact_mn);
+        lNameEdit = findViewById(R.id.txt_contact_ln);
+
+        fName.setVisibility(View.VISIBLE);
+        mName.setVisibility(View.VISIBLE);
+        lName.setVisibility(View.VISIBLE);
+
+        fNameEdit.setVisibility(View.GONE);
+        mNameEdit.setVisibility(View.GONE);
+        lNameEdit.setVisibility(View.GONE);
+
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fName.setVisibility(View.GONE);
+                mName.setVisibility(View.GONE);
+                lName.setVisibility(View.GONE);
+
+                fNameEdit.setVisibility(View.VISIBLE);
+                mNameEdit.setVisibility(View.VISIBLE);
+                lNameEdit.setVisibility(View.VISIBLE);
+
+                edit.setText("Save");
+                edit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(ContactInformation.this, "Save" , Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/lcgg/lcggpay/ui/profile/LoginCredentials.java
+++ app/src/main/java/com/lcgg/lcggpay/ui/profile/LoginCredentials.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,13 +14,21 @@ import com.lcgg.lcggpay.Login;
 import com.lcgg.lcggpay.R;
 
 public class LoginCredentials extends AppCompatActivity {
-    Intent intent;
-    private FirebaseAuth mAuth;
+    Button edit;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_credential);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        edit = findViewById(R.id.loginBtn_edit);
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
